a1f2225667229593213dc5e5c0f63a40848abaf3
Viraj Chandra <virajchandra51@gmail.com>
2022-12-24 23:41:54 +0530

    about-us

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,12 +3,14 @@ import Navbar from '../../components/navbar/Navbar'
 import './HomePage.css';
 import '../../App.css';
 import Section1 from '../../sections/section1/Section1';
+import Section2 from '../../sections/section2/Section2';
 export default class HomePage extends Component {
   render() {
     return (
       <div className="container mx-auto">
         <Navbar/>
         <Section1/>
+        <Section2/>
       </div>
     )
   }
