0d7a1effce38bddb52bfe709b0e9f1b3cf88d3dc
Aniket Kumar <kraniket123654@gmail.com>
2022-12-25 21:32:46 +0530

    added EventCard component and eventsData.js + Made few changes in section 3

--- src/sections/section3/Section3.js
+++ src/sections/section3/Section3.js
@@ -1,27 +1,48 @@
-import React, { Component } from 'react'
-import './Section3.css';
-import Event from '../../components/event/Event';
+import React from 'react'
 
-export default class Section1 extends Component {
-  render() {
-    return (
-    <div className="codeutsava__section3" id="events">
-        <div className="codeutsava__section3-body">
-            <div className="codeutsava__section3-title">
-                Events
-            </div>
-            <div className="codeutsava__section3-menu">
-                <Event/>
-                <Event/>
-                <Event/>
-                <Event/>
-            </div>
-            <div className="codeutsava__section3-slider">
-                <div className="larr">&larr;</div>
-                <div className="rarr">&rarr;</div>
+import './Section3.css'
+
+import events from '../../assets/data/eventsData.js'
+import leftArrow from '../../assets/images/leftArrow.png'
+import rightArrow from '../../assets/images/rightArrow.png'
+
+import EventCard from '../../components/eventCard/EventCard'
+
+const Section3 = () => {
+    const scrollRef = React.useRef(null);
+    const scroll = (direction) => {
+        const { current } = scrollRef;
+
+        if (direction === 'left') {
+        current.scrollLeft -= 300;
+        } else {
+        current.scrollLeft += 300;
+        }
+    };
+    return(
+        <div className="codeutsava__section3" id="events">
+            <div className="codeutsava__section3-body">
+                <div className="codeutsava__section3-title">
+                    Events
+                </div>
+                <div className="codeutsava__section3-events">
+                    <div className="codeutsava__section3-events-container" ref={scrollRef}>
+                    {events.map((event,index) => (
+                        <EventCard img={event.img} title={event.title} date={event.date}/>
+                    ))}    
+                    </div>
+                </div>
+                <div className="codeutsava__section3-slider">
+                    <div className="codeutsava__section3-slider-left" onClick={() => scroll('left')}>
+                        <img src={leftArrow} alt="leftArrow"></img>
+                    </div>
+                    <div className="codeutsava__section3-slider-right" onClick={() => scroll('right')}>
+                        <img src={rightArrow} alt="rightArrow"></img>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    )
-  }
-}
+    );
+};
+
+export default Section3;
