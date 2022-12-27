263e9bfea95f9cd15ae1e00b8c494c1e00a62e30
Bhupendra-Gehlot1 <gehlotbhopesh007@gmail.com>
2022-12-27 15:11:32 +0530

    added Sponsors and speakers section in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -19,6 +19,9 @@ export default class Navbar extends Component {
             <a href='#events'><div className='codeutsava__navbar-menu-heading'>
                     Events
                 </div></a>
+            <div className='codeutsava__navbar-menu-heading'>
+                    Sponsors
+            </div>
             <a href='#speakers'><div className='codeutsava__navbar-menu-heading'>
                     Speakers
                 </div></a>
