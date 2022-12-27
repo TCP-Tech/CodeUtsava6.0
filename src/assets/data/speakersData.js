bd2fbc775d1affb4adc69d90405553d9310df0e7
Bhupendra-Gehlot1 <gehlotbhopesh007@gmail.com>
2022-12-27 13:33:37 +0530

    completed

--- src/assets/data/speakersData.js
+++ src/assets/data/speakersData.js
@@ -31,6 +31,27 @@ const speakers = [
     linkedin: "",
     github: "",
   },
+  {
+    name: "Amarnath Sharma",
+    position: "Software Engineer Google",
+    description: "I choose product design because i love seeing beautiful interfaces and to be able to solve user’s problems.",
+    img: speaker1,
+    facebook: "",
+    twitter: "",
+    linkedin: "",
+    github: "",
+  },
+  {
+    name: "Amarnath Sharma",
+    position: "Software Engineer Google",
+    description: "I choose product design because i love seeing beautiful interfaces and to be able to solve user’s problems.",
+    img: speaker1,
+    facebook: "",
+    twitter: "",
+    linkedin: "",
+    github: "",
+  },
+  // extra for vertical positioning of cards
   
 ];
 
