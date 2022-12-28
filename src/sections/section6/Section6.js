1b2758855bd5db24c97493f22319c3a70366c291
Viraj Chandra <95118745+virajchandra51@users.noreply.github.com>
2022-12-28 19:00:30 +0530

    Update Section6.js

--- src/sections/section6/Section6.js
+++ src/sections/section6/Section6.js
@@ -6,42 +6,42 @@ import sponsers from "../../assets/data/sponsersData.js";
 
 const Section6 = () => {
   return (
-    <div className="codeutsava__section5" id="sponsers">
-      <div className="codeutsava__section5-body">
-        <div className="codeutsava__section5-title">Sponsers</div>
-        <div className="codeutsava__section5-content">
+    <div className="codeutsava__section6" id="sponsers">
+      <div className="codeutsava__section6-body">
+        <div className="codeutsava__section6-title">Sponsers</div>
+        <div className="codeutsava__section6-content">
           On behalf of team TCP, we would like to publicly thank the generous
           sponsors without whom this event could not be held so smoothly.
         </div>
-        <div className="codeutsava__section5-sponsers">
-          <div className="codeutsava__section5-sponser-title">
+        <div className="codeutsava__section6-sponsers">
+          <div className="codeutsava__section6-sponser-title">
             Platinum Sponsors
           </div>
-          <div className="codeutsava__section5-sponser-list platinum">
+          <div className="codeutsava__section6-sponser-list platinum">
             {sponsers.platinum.map((sponser, index) => (
               <img src={sponser.img} />
             ))}
           </div>
-          <div className="codeutsava__section5-sponser-title">
+          <div className="codeutsava__section6-sponser-title">
             Gold Sponsors
           </div>
-          <div className="codeutsava__section5-sponser-list gold">
+          <div className="codeutsava__section6-sponser-list gold">
             {sponsers.gold.map((sponser, index) => (
               <img src={sponser.img} />
             ))}
           </div>
-          <div className="codeutsava__section5-sponser-title">
+          <div className="codeutsava__section6-sponser-title">
             Silver Sponsors
           </div>
-          <div className="codeutsava__section5-sponser-list silver">
+          <div className="codeutsava__section6-sponser-list silver">
             {sponsers.silver.map((sponser, index) => (
               <img src={sponser.img} />
             ))}
           </div>
-          <div className="codeutsava__section5-sponser-title">
+          <div className="codeutsava__section6-sponser-title">
             Bronze Sponsors
           </div>
-          <div className="codeutsava__section5-sponser-list bronze">
+          <div className="codeutsava__section6-sponser-list bronze">
             {sponsers.bronze.map((sponser, index) => (
               <img src={sponser.img} />
             ))}
