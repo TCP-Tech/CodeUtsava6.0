7def305c90f743497e66e4c697b6af7a84d2d665
Shreya Pandey <105601918+Olshia@users.noreply.github.com>
2023-01-06 00:03:36 +0530

    Added section 9 files

---
+++ src/sections/section9/Section9.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Prizes from '../../assets/data/PrizesData';
+import PrizeBox from "../../components/PrizesBox/Prizebox";
+
+import './Section9.css';
+
+const Section9 = ()=> {
+
+    return (
+        <div className="codeutsava_section9">
+            <div className="codeutsava_section9-title">
+                <div className="codeutsava_section9-title1">Prizes</div>
+                <div className="codeutsava_section9-title2">Win excitinng prizes worth upto 8 lakhs</div>
+            </div>
+            <div className="codeutsava_section9-body">
+                <div className="codeutsava_section9-description">
+                    {Prizes.map((info , index ) => (
+                        <PrizeBox title = {info.title} prizeInfo = {info.prizeInfo} cashPrize = {info.cashPrize} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+
+};
+
+export default Section9;
