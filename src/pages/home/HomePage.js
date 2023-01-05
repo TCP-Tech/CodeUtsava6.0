ba020d18d29ba7162a23877314d9989e526c2024
Shreya Pandey <105601918+Olshia@users.noreply.github.com>
2023-01-06 00:09:10 +0530

    Updated Homepage

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -8,6 +8,7 @@ import Section3 from '../../sections/section3/Section3';
 import Section4 from '../../sections/section4/Section4';
 import Section6 from '../../sections/section6/Section6';
 import Section7 from '../../sections/section7/Section7';
+import Section9 from '../../sections/section9/Section9';
 export default class HomePage extends Component {
   render() {
     return (
@@ -19,6 +20,7 @@ export default class HomePage extends Component {
         <Section4/>
         <Section6/>
         <Section7/>
+        <Section9/>
       </div>
     )
   }
