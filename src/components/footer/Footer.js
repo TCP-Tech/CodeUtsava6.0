3d4b17a46f7ac72787c31fba9adc1513b1709d97
Bhupendra-Gehlot1 <gehlotbhopesh007@gmail.com>
2023-01-17 22:06:04 +0530

    Meet my team section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.css";
 import NIT from "../../assets/images/NIT.svg";
 import tcpName from "../../assets/images/tcpName.svg";
 import codeutsava from "../../assets/images/codeutsava.png";
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -117,10 +117,9 @@ const Footer = () => {
         </div>
       </div>
       <div className="codeutsava__footer-container3">
-        <div className="codeutsava__footer-social-team">
+        {/* <div className="codeutsava__footer-social-team">
           Architected with ❤️ by <Link to="/team">TCP Team</Link>
-
-        </div>
+        </div> */}
         <div className="codeutsava__footer-social-map">
           <a target="_blank" href="https://www.google.com/maps/place/National+Institute+of+Technology(NIT),+Raipur,+Great+Eastern+Rd,+Amanaka,+Raipur,+Chhattisgarh+492010/@21.2497222,81.6050291,17z/data=!4m6!3m5!1s0x3a28dde213f66723:0x21543965c50c43c7!8m2!3d21.2497222!4d81.6050291!16zL20vMGNqeXFk">
             <svg
---
+++ src/sections/section10/Section10.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Section10.css';
+
+import {Link} from 'react-router-dom'
+
+const section10 = () => {
+  return (
+    <div className="codeutsava__section10" id="problems">
+    <div className="codeutsava__section10-body">
+      <div className="codeutsava__section10-title">Wondering who all make this possible?</div>
+      <button
+          className="codeutsava__section10-button"><Link to="/team">Meet our Team</Link>
+        </button>
+      </div>
+      </div>
+  )
+}
+
+export default section10
