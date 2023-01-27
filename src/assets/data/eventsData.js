a5a7649740b23820d3d9260b169ab521cd5972db
Viraj Chandra <virajchandra51@gmail.com>
2023-01-27 17:36:41 +0530

    new event

--- src/assets/data/eventsData.js
+++ src/assets/data/eventsData.js
@@ -1,22 +1,32 @@
 import event1 from '../images/event1.png'
 import event2 from '../images/event2.png'
 import event3 from '../images/event3.png'
+import event4 from '../images/event4.png'
 
 const events = [
   {
     title: "Cloud Techniques and Placement Tips",
     date: "14th and 15th January",
     img: event1,
+    link:"",
   },
   {
     title: "28 Hours Hackathon",
     date: "11th and 12th February",
     img: event2,
+    link:"",
   },
   {
     title: "CP and Algo Bootcamp",
     date: "21st and 22nd January",
     img: event3,
+    link:"",
+  },
+  {
+    title: "AI and Machine Learning",
+    date: "28th and 29th January",
+    img: event4,
+    link:"https://forms.gle/zetADszoQMTad7JV8",
   },
 ];
 
